0aea9fb1d240bc6de9fb9dbafcd65fdf7d226943
Evgenii Li <evgeniili2203@gmail.com>
2023-08-09 01:44:08 +0300

    Add solution for test_readable_function

--- homework.py
+++ homework.py
@@ -88,6 +88,22 @@ def test_find_suitable_user():
 # сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
+def readable_function(func, *args):
+    result = ''
+    prev_char = ''
+    for index, char in enumerate(func.__name__):
+        if index == 0:
+            result = result + char.upper()
+        elif prev_char == '_':
+            result = result + char.upper()
+        elif char == '_':
+            result = result + ' '
+        else:
+            result = result + char
+        prev_char = char
+    result = f'{result} [{", ".join(args)}]'
+    print(result)
+    return result
 
 
 def test_readable_function():
@@ -97,15 +113,15 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = readable_function(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = readable_function(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = readable_function(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
